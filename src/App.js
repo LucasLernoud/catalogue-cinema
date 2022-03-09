239a2153ed6c5a35afabf2755cd081fdcc5e5207
Lucas Lernoud <lucaslerdb@gmail.com>
2022-03-09 14:50:49 +0100

    navigation added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import UserList from './pages/UserList'
 
 const App = () => {
   return (
-    <div>
-      <h1>test</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favoris" element={<UserList />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
