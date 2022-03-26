6a251144c5331eae9fb9b41537176f8ab2396e89
Lucas Lernoud <lucaslerdb@gmail.com>
2022-03-26 14:12:55 +0100

    affichage des favoris ok. à faire : supprimer un favori de la liste

--- src/pages/UserList.js
+++ src/pages/UserList.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import Header from '../components/Header';
+import axios from "axios";
+import React from "react";
+import { useState, useEffect } from "react";
+import Header from "../components/Header";
+import Card from "../components/Card";
 
 const UserList = () => {
-    return (
-        <div>
-            <Header />
-        </div>
-    );
+  const [listData, setListData] = useState([]);
+
+  useEffect(() => {
+    let moviesId = window.localStorage.movies
+      ? window.localStorage.movies.split(",")
+      : [];
+
+    for (let i = 0; i < moviesId.length; i++) {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=b4d15bc97662697e2534d61b733bf5a7&language=fr-FR`
+        )
+        .then((res) => setListData((listData) => [...listData, res.data]));
+    }
+  }, []);
+
+  return (
+    <div className="user-list-page">
+      <Header />
+      <h2>
+        Vos films favoris <span>💘</span>
+      </h2>
+      <div className="result">
+        {listData.length > 0 ? (
+          listData.map((movie) => <Card movie={movie} key={movie.id} />)
+        ) : (
+          <h2>Aucun coup de coeur pour le moment</h2>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default UserList;
