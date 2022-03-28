b182118091d2e902659676c858ad752ccf56a735
Lucas Lernoud <lucaslerdb@gmail.com>
2022-03-28 14:49:38 +0200

    logique finie reste à changer le style

--- src/components/Card.js
+++ src/components/Card.js
@@ -85,6 +85,13 @@ const Card = ({ movie }) => {
     }
   };
 
+  const deleteStorage = () => {
+    let storedData = window.localStorage.movies.split(",");
+    let newData = storedData.filter((id) => id != movie.id);
+    window.localStorage.movies = newData;
+    window.location.reload();
+  }
+
   return (
     <div className="card">
       <img
@@ -113,9 +120,13 @@ const Card = ({ movie }) => {
       </ul>
       {movie.overview ? <h3>Synopsis</h3> : ""}
       <p>{movie.overview}</p>
-      <div className="btn" onClick={() => addStorage()}>
-        Ajouter aux favoris
-      </div>
+      {movie.genre_ids ? (
+        <div className="btn" onClick={() => addStorage()}>
+          Ajouter aux favoris
+        </div>
+      ) : (
+        <div className="btn" onClick={() => deleteStorage()}>Supprimer de la liste</div>
+      )}
     </div>
   );
 };
